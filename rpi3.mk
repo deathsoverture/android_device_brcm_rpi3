9903d2948eacedbd60580332d8881a8a0fe60e2a
secuflag <secuflag@ticino.com>
2016-06-26 08:10:24 +0200

    Fix bluetooth
     * Enable A2DP
    
    Signed-off-by: secuflag <secuflag@ticino.com>

--- rpi3.mk
+++ rpi3.mk
@@ -13,6 +13,7 @@ PRODUCT_PACKAGES += \
     libGLES_mesa \
     gralloc.$(TARGET_PRODUCT) \
     hwcomposer.$(TARGET_PRODUCT) \
+    audio.a2dp.default \
     audio.primary.$(TARGET_PRODUCT) \
     wpa_supplicant \
     wpa_supplicant.conf
